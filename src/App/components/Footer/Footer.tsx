e3df1a13a5f04d2ddf3ab324c8e265281fadb256
VictorM <neuron96@mail.ru>
2023-06-16 19:28:45 +0500

    fix: centered footer logo

--- src/App/components/Footer/Footer.tsx
+++ src/App/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import FooterInfo from '../FooterInfo/FooterInfo';
 const Footer = () => {
   return (
     <footer className="mt-[129px] bg-footer-bg  ">
-      <div className="flex justify-between pt-[35px] pl-[84px] pr-[79px]">
-        <h1 className="font-arimo text-5xl text-white">LOGO</h1>
+      <div className="flex justify-between  pt-[35px] pl-[84px] pr-[79px]">
+        <h1 className="font-arimo text-5xl flex items-center text-white">LOGO</h1>
 
         <FooterInfo title="Company name" info={['Reg. nr: 437568', 'Peterburi tee 710', 'Tallinn Harjumaa 34235']} />
         <FooterInfo
